4f9952e91b425df938fbeaa9280318422750084b
IDV <ivakin_danya@mail.ru>
2024-04-24 11:15:15 +0300

    Создал проект, настроил зависимости, настроил базу и подключение к ней

---
+++ src/main/java/ds/dsinternshipcontrolsystem/DsInternshipControlSystemApplication.java
@@ -0,0 +1,13 @@
+package ds.dsinternshipcontrolsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsInternshipControlSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DsInternshipControlSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ds/dsinternshipcontrolsystem/DsInternshipControlSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package ds.dsinternshipcontrolsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsInternshipControlSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
